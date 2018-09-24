23da13f1e0c7ade87327b8a95defc4f942b460dd
Harold <pxduharold@126.com>
2018-09-24 11:12:37 +0800

    Init commit

---
+++ hellpwworld_project/hellpwworld_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hellpwworld_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hellpwworld_project.settings')
+
+application = get_wsgi_application()
